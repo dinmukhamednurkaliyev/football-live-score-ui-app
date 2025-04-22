947689281cc245bad0aac75adb0161c34dd36c00
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-22 19:06:43 +0500

    feat(ui): develop home screen UI with custom app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:football_live_score_ui_app/theme/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,92 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          const SizedBox(width: 20),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Material(
+              elevation: 0.2,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Icon(Icons.window_rounded),
+              ),
+            ),
+          ),
+          const Spacer(),
+          const Row(
+            children: [
+              Text(
+                'S',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                  letterSpacing: -2,
+                ),
+              ),
+              Icon(
+                Icons.sports_soccer,
+                color: AppColors.kprimaryColor,
+                size: 25,
+              ),
+              Text(
+                'ccer',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  letterSpacing: -2,
+                ),
+              ),
+              Text(
+                'Nerds',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: AppColors.kprimaryColor,
+                  letterSpacing: -2,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Material(
+              elevation: 0.2,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Stack(
+                  children: [
+                    Icon(Icons.notifications),
+                    Positioned(
+                      right: 1,
+                      child: CircleAvatar(
+                        radius: 4,
+                        backgroundColor: AppColors.kprimaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,63 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:football_live_score_ui_app/screens/home_screen.dart';
 import 'package:football_live_score_ui_app/theme/colors.dart';
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int currentTab = 0;
-  final List<Widget> screens = [
-    const HomeScreen(),
-    const Scaffold(),
-    const Scaffold(),
-    const Scaffold(),
-  ];
-  final icons = [
-    Icons.home_outlined,
-    Icons.calendar_month_outlined,
-    Icons.area_chart_rounded,
-    Icons.person_outline,
-  ];
-  final titles = ['Home', 'Calendar', 'Standing', 'Account'];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Container(
-        height: 80,
-        padding: const EdgeInsets.only(top: 10),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromRGBO(0, 0, 0, 0.015),
-              blurRadius: 8,
-              spreadRadius: 5,
-            ),
-          ],
-          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: List.generate(
-            4,
-            (index) => BottomNavBarItems(
-              title: titles[index],
-              icon: icons[index],
-              isActive: currentTab == index,
-              onTab: () => setState(() => currentTab = index),
-            ),
-          ),
-        ),
-      ),
-      body: screens[currentTab],
-    );
-  }
-}
-
 class BottomNavBarItems extends StatelessWidget {
   const BottomNavBarItems({
     required this.title,
@@ -94,6 +37,63 @@ class BottomNavBarItems extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentTab = 0;
+  final List<Widget> screens = [
+    const HomeScreen(),
+    const Scaffold(),
+    const Scaffold(),
+    const Scaffold(),
+  ];
+  final icons = [
+    Icons.home,
+    Icons.calendar_month,
+    Icons.bar_chart,
+    Icons.person,
+  ];
+  final titles = ['Home', 'Calendar', 'Standing', 'Account'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 80,
+        padding: const EdgeInsets.only(top: 10),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.015),
+              blurRadius: 8,
+              spreadRadius: 5,
+            ),
+          ],
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(
+            4,
+            (index) => BottomNavBarItems(
+              title: titles[index],
+              icon: icons[index],
+              isActive: currentTab == index,
+              onTab: () => setState(() => currentTab = index),
+            ),
+          ),
+        ),
+      ),
+      body: screens[currentTab],
     );
   }
 }
