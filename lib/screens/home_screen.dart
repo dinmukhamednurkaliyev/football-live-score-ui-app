1ecbc6d5f4a0c4f6a0060b29477d1ac084fc4c51
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-23 01:15:26 +0500

    refactor(ui, widgets): extract live match item UI into dedicated widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_live_score_ui_app/models/live_match_model.dart';
 import 'package:football_live_score_ui_app/theme/colors.dart';
+import 'package:football_live_score_ui_app/widgets/live_match.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -29,144 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 final live = liveMatches[index];
                 return GestureDetector(
                   onTap: () {},
-                  child: Container(
-                    height: 230,
-                    margin: const EdgeInsets.only(right: 20),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      color: live.color,
-                      borderRadius: BorderRadius.circular(35),
-                      image: live.backgroundImage,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          live.stadium,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: live.textColors,
-                            letterSpacing: -1,
-                          ),
-                        ),
-                        Text(
-                          'Week 13',
-                          style: TextStyle(
-                            color: live.textColors,
-                            letterSpacing: -1,
-                            fontSize: 11,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Column(
-                              children: [
-                                Image.asset(
-                                  live.homeLogo,
-                                  height: 90,
-                                  width: 90,
-                                ),
-                                const SizedBox(height: 10),
-                                Text(
-                                  live.homeTitle.toUpperCase(),
-                                  style: TextStyle(
-                                    color: live.textColors,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    letterSpacing: -1,
-                                  ),
-                                ),
-                                Text(
-                                  'Home',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    color: live.textColors,
-                                    letterSpacing: -1,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(width: 20),
-                            Column(
-                              children: [
-                                const SizedBox(height: 5),
-                                Text(
-                                  "${live.time}'",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: live.textColors,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: '${live.homeGoal} : ',
-                                        style: TextStyle(
-                                          fontSize: 36,
-                                          fontWeight: FontWeight.bold,
-                                          color:
-                                              live.onTheWinner
-                                                  ? AppColors.kprimaryColor
-                                                  : live.textColors,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text: live.awayGoal.toString(),
-                                        style: TextStyle(
-                                          fontSize: 36,
-                                          color:
-                                              live.onTheWinner
-                                                  ? AppColors.kprimaryColor
-                                                  : live.textColors,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(width: 20),
-                            Column(
-                              children: [
-                                Image.asset(
-                                  live.awayLogo,
-                                  height: 90,
-                                  width: 90,
-                                ),
-                                const SizedBox(height: 10),
-                                Text(
-                                  live.homeTitle.toUpperCase(),
-                                  style: TextStyle(
-                                    color: live.textColors,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    letterSpacing: -1,
-                                  ),
-                                ),
-                                Text(
-                                  'Away',
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    color: live.textColors,
-                                    letterSpacing: -1,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: LiveMatchData(live: live),
                 );
               },
             ),
